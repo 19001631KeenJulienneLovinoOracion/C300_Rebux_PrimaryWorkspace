9db774ededfa7296e3ee05db39e2e295a397267f
YEE CHENG HAN RP <19014273@myrp.edu.sg>
2021-05-11 10:34:43 +0800

    Testt

---
+++ C300_Rebux_PrimaryWorkspace/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C300_Rebux_PrimaryWorkspace.Models
+{
+    public class Class
+    {
+    }
+}
