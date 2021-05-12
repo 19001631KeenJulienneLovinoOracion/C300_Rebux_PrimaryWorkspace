aac43bab4de696f1e1fe77aa3778f98ade9689cf
BENEDIC TAN MING HAO RP <19012079@myrp.edu.sg>
2021-05-12 12:45:09 +0800

    created controllers for website and kiosk

---
+++ C300_Rebux_PrimaryWorkspace/Controllers/KioskController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C300_Rebux_PrimaryWorkspace.Controllers
+{
+    public class KioskController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
---
+++ C300_Rebux_PrimaryWorkspace/Controllers/WebsiteController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C300_Rebux_PrimaryWorkspace.Controllers
+{
+    public class WebsiteController : Controller
+    {
+        public IActionResult MainMenu()
+        {
+            return View();
+        }
+    }
+}
