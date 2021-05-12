a1ac6c033e9bfc76cba121e0fe930ec7e842c20f
KEEN JULIENNE LOVINO ORACION RP <19001631@myrp.edu.sg>
2021-05-12 12:55:49 +0800

    I created a KeenController

---
+++ C300_Rebux_PrimaryWorkspace/Controllers/KeenController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C300_Rebux_PrimaryWorkspace.Controllers
+{
+    public class KeenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
